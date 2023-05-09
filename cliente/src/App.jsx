5b4e1a6c3b58bbfc4e75b40cdb2a10e1380cd82c
dimwarlock <104273605+dimwarlock@users.noreply.github.com>
2023-05-09 11:57:46 -0400

    Update App.jsx

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" Component={FrontPage} />
+        <RouterProvider Component={FrontPage} />
         <Route exact path="/administrador" Component={Administrador} />
       </Routes>
     </Router>
