3dc745e14bca17730a56a1fa1968908107fc0e22
dimwarlock <104273605+dimwarlock@users.noreply.github.com>
2023-05-09 11:50:33 -0400

    Update index.js

--- cliente/src/index.js
+++ cliente/src/index.js
@@ -5,7 +5,7 @@ import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App/>
-  </React.StrictMode>
+  </BrowserRouter>
 );
